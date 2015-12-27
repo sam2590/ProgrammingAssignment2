49e6b0e372dcdd33381db5b254002d1ca659f86e
sam2590 <joelsthomson@gmail.com>
2015-12-27 17:36:03 -0500

    Latest commit for ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -30,7 +30,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x,...) {
       
-	m <- x$getinverse 
+	m <- x$getinverse()
 	if(!is.null(m)) {
 		message("getting cached data")
 		return(m)
